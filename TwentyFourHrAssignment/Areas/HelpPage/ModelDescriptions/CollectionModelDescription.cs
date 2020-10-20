37c0a9442473344fd52f27ac79da2e954fa2ed0e
BHollis1 <betsy.hollis1@gmail.com>
2020-10-20 09:26:20 -0400

    Add project files.

---
+++ TwentyFourHrAssignment/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TwentyFourHrAssignment.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TwentyFourHrAssignment/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TwentyFourHrAssignment.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
